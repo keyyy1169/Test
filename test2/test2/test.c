26544d864fe6acf4db001d5add91daf28959492e
keyyy1169 <3184753692@qq.com>
2024-07-25 16:17:26 +0800

    Add files via upload

---
+++ test2/test2/test.c
@@ -0,0 +1,60 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int main()
+//{
+//	//printf("d\test\abc\navel");
+//	//printf("d\\aestb\\babc\\bavel");
+//	//printf ("ab\ncd");
+//	//printf("(abc!\?\?)");
+//	//printf("%c",'\'');
+//	//printf("%s\n","abc");
+//	//printf("%s\n","a");
+//	//printf("%c\n","a");//应为单引号
+//	printf("%c\n",'b');
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("%c\n",'\120');
+//	printf("%c\n",'\x17');
+//	printf("%c", '\101');
+//	printf("%c", '\x55');
+//	
+//	return 0;
+//}
+
+
+//int main()
+///* {
+//	/*int a = 100;
+//	printf("%d", a);
+//	return 0;*/
+//}*/
+
+
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	
+//	if (a > 15)
+//	    printf("too big");
+//	if (a < 15)
+//		printf("too small");
+//	if (a == 15)
+//		printf("good");
+//
+//	return 0;
+//}
+
+
