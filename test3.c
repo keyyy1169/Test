ffa8e91a66410786ce2a540e3fcd9badc3890a02
keyyy1169 <3184753692@qq.com>
2024-07-29 21:21:54 +0800

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,99 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	
+//	if (a > 15)
+//	    printf("too big");
+//	if (a < 15)
+//		printf("too small");
+//	if (a == 15)
+//		printf("good");
+//
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	int a = 0;
+//	while (a < 30000)
+//	{
+//		printf("¼ÌÐø:%d\n", a);
+//		a++;
+//	}
+//	if (a == 30000)
+//	{
+//		printf("³É¹¦");
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int add = a + b;
+//	printf("%d", add);
+//	return 0;
+//}
+
+
+
+//int add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int sum = add(a, b);
+//	printf("%d", sum);
+//	
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int a = 0;
+//	while (a < 10)
+//	{
+//		printf("%d\n", arr[a]);
+//		a++;
+//	}
+//	if (a == 10)
+//	{
+//		printf("arr[a]");
+//	}
+//	
+//	return 0;
+//}
+
+
+
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d", arr[5]);
+	return 0;
+}
